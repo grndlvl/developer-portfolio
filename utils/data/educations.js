9f8381c1d48f61c9144e5cf4e8d31db209b0e1b2
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2025-07-18 22:36:07 -0400

    Updating education and experiences.

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,13 +2,13 @@ export const educations = [
   {
     id: 1,
     title: 'Bachelor of Science in Computer Science, Certificate in Cyber Defender; GPA: Cum Laude',
-    duration: 'Aug 2007 -- May 2018',
+    duration: '(Aug 2007 - May 2018)',
     institution: 'Augusta University',
   },
   {
     id: 2,
     title: 'Associate of Applied Science in Computer Programming',
-    duration: 'Aug 2004 -- May 2007',
+    duration: '(Aug 2004 - May 2007)',
     institution: 'Augusta Technical College',
   },
 ];

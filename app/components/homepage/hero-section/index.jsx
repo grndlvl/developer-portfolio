237bbd39339a246f236b19ecb4c007d232bf9ff7
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2025-07-19 09:40:46 -0400

    Adding Typewriter effect for hero.

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -1,8 +1,9 @@
 // @flow strict
-
+'use client';
 import { personalData } from "@/utils/data/personal-data";
 import Image from "next/image";
 import Link from "next/link";
+import Typewriter from 'typewriter-effect';
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { SiDrupal } from "react-icons/si";
 import { FaFacebook, FaTwitterSquare } from "react-icons/fa";
@@ -11,6 +12,8 @@ import { RiContactsFill } from "react-icons/ri";
 import { SiLeetcode } from "react-icons/si";
 
 function HeroSection() {
+  const cleanedDesignation = personalData.designation.replace('&', '</span> & <span class="text-[#16f2b3]">');
+
   return (
     <section className="relative flex flex-col items-center justify-between py-4 lg:py-12">
       <Image
@@ -24,12 +27,28 @@ function HeroSection() {
       <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
           <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
-            Hello, <br />
-            This is {' '}
-            <span className=" text-pink-500">{personalData.name}</span>
-            {` , I'm a Professional `}
-            <span className=" text-[#16f2b3]">{personalData.designation}</span>
-            .
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString('Hello, <br /> This is <span class="text-pink-500">' + `${personalData.name}` + "</span>, I'm a Professional ")
+                  .typeString('<span class="text-[#16f2b3]">CTO</span>.')
+                  .pauseFor(1000)
+                  .deleteChars(4)
+                  .typeString('<span class="text-[#16f2b3]">Software Engineer building scalable, AI-powered systems</span>.')
+                  .pauseFor(1000)
+                  .deleteChars(56)
+                  .typeString('<span class="text-[#16f2b3]">Infrastructure Architect for high-growth startups</span>.')
+                  .pauseFor(1000)
+                  .deleteChars(50)
+                  .typeString('<span class="text-[#16f2b3]">Remote Team Leader turning ideas into shipped products</span>.')
+                  .pauseFor(1000)
+                  .start();
+              }}
+              options={{
+                autoStart: true,
+                loop: true,
+              }}
+            />
           </h1>
 
           <div className="my-12 flex items-center gap-5">

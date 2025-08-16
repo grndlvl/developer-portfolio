dc1610d7c96d24e226944860d6357ce2cb0f608f
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2025-08-16 08:43:41 -0400

    Use Google Voice phone number.

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
   email: 'delaigle.jonathan@gmail.com',
-  phone: '+1 706-589-6189',
+  phone: '+1 803-599-6443',
   github: 'https://github.com/grndlvl',
   linkedIn: 'https://www.linkedin.com/in/delaigle',
 };

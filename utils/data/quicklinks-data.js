1b7412e1d61037fd17b52a187afd2dc6bb44d5b9
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2025-09-11 16:38:45 -0400

    Adding k8s instructional playlist.

--- utils/data/quicklinks-data.js
+++ utils/data/quicklinks-data.js
@@ -1,26 +1,22 @@
 // Data for custom quick links to display on Linktree-inspired page
 // Update this list with your own quick links (e.g., blog, projects, resume)
 import { personalData } from './personal-data';
-import { FaBlog, FaFilePdf, FaProjectDiagram } from 'react-icons/fa';
+import { FaFilePdf } from 'react-icons/fa';
+import { SiKubernetes } from "react-icons/si";
 
 /**
  * Array of quick link items for Linktree-inspired page.
  * Each item can include an icon component, a display name, and a URL.
  */
 export const quickLinks = [
-//  {
-//    name: 'Blog',
-//    url: '/blog',
-//    icon: FaBlog,
-//  },
-//  {
-//    name: 'Projects',
-//    url: '/projects',
-//    icon: FaProjectDiagram,
-//  },
   {
     name: 'Resume',
     url: personalData.resume,
     icon: FaFilePdf,
   },
+  {
+    name: 'Kubernetes Starter: Ingress, Probes & Autoscaling',
+    url: 'https://www.youtube.com/playlist?list=PLG_KgaoqTvlqIx03sPeG8axsZ-H4jtQ5W',
+    icon: SiKubernetes,
+  }
 ];

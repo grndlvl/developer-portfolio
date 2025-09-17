19e3da1d5240532b27458ca70d545e1f72a543de
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2025-09-17 05:40:43 -0400

    Adding redirect for "React Won by Default – And It's Killing Frontend Innovation"

--- utils/data/redirectsData.js
+++ utils/data/redirectsData.js
@@ -2,5 +2,6 @@
 export const redirectsData = {
   // Example mapping: slug => domain.com/super-long-path-that-needs-a-slug
   'k8s-starter': 'https://youtube.com/playlist?list=PLG_KgaoqTvlqIx03sPeG8axsZ-H4jtQ5W&si=e6iwJ_5OGjTEa_T5',
+  'react-won': 'https://www.lorenstew.art/blog/react-won-by-default/',
   // Add more mappings here as needed
 };

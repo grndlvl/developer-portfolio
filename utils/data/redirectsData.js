0616b5eb244b4dea09af04d742a8fac39e069d99
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2025-09-18 12:57:11 -0400

    Adding boring-is-good redirect

--- utils/data/redirectsData.js
+++ utils/data/redirectsData.js
@@ -1,6 +1,7 @@
 // Add short URL mappings: slug => destination URL
 export const redirectsData = {
   // Example mapping: slug => domain.com/super-long-path-that-needs-a-slug
+  'boring-is-good': 'https://jenson.org/boring/?utm_source=grndlvl&utm_medium=redirect',
   'k8s-starter': 'https://youtube.com/playlist?list=PLG_KgaoqTvlqIx03sPeG8axsZ-H4jtQ5W&si=e6iwJ_5OGjTEa_T5',
   'react-won': 'https://www.lorenstew.art/blog/react-won-by-default/?utm_source=grndlvl&utm_medium=redirect&utm_campaign=react_won_carousel',
   // Add more mappings here as needed

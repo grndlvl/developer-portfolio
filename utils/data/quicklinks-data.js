ac1c62503c74ab5f5c1d59d866763dd021e4074d
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2025-09-19 18:38:57 -0400

    Adding tech pickupline playlist.

--- utils/data/quicklinks-data.js
+++ utils/data/quicklinks-data.js
@@ -1,5 +1,6 @@
 // Data for custom quick links to display on Linktree-inspired page
 // Update this list with your own quick links (e.g., blog, projects, resume)
+import { FaCar } from "react-icons/fa";
 import { personalData } from './personal-data';
 import { SiKubernetes } from "react-icons/si";
 
@@ -8,6 +9,11 @@ import { SiKubernetes } from "react-icons/si";
  * Each item can include an icon component, a display name, and a URL.
  */
 export const quickLinks = [
+  {
+    name: 'Tech Pickup Line',
+    url: 'https://www.youtube.com/playlist?list=PLG_KgaoqTvlr6a5jcbZ97bCQ_XXaTzogx',
+    icon: FaCar,
+  },
   {
     name: 'Kubernetes Starter: Ingress, Probes & Autoscaling',
     url: 'https://www.youtube.com/playlist?list=PLG_KgaoqTvlqIx03sPeG8axsZ-H4jtQ5W',
